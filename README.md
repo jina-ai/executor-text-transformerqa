ce826752ddd34d3b4f22e837f6da5457de96ca09
Winston Wong <winstonwongww2@gmail.com>
2022-02-07 12:13:50 +0800

    feat: add transformer qa executor (#7)
    * feat: empty branch
    
    * feat: add transformer qa executor
    
    * refactor: address comments
    
    * refactor: address comments
    
    * fix: typo in readme
    
    * feat: parametrize traversal_paths
    
    * docs: add default_traversal_paths docstr

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # TransformerQAExecutor
 
-**TransformerQAExecutor** wraps a question-answering model from huggingface. Given some questions and paragraphs/contexts, it extracts the relevant answers from the given paragraphs/contexts.
+**TransformerQAExecutor** wraps a question-answering model from Hugging Face. Given a question and paragraph/context, it extracts the relevant answers from the given paragraph/context.
 
-**TransformerQAExexecutor** receives [`Document`s](https://docs.jina.ai/fundamentals/document/) where each `Document`'s `text` is the question, and its `matches`'s texts are the contexts/paragraphs where answers will be extracted from.
+**TransformerQAExexecutor** receives [`Document`s](https://docs.jina.ai/fundamentals/document/) where each `Document`'s `text` is the paragraph, `doc.tags['question']` stores the question.
 
 ## Usage
 
@@ -14,26 +14,22 @@ from jina import Flow, Document
 f = Flow().add(uses='jinahub+docker://TransformerQAExecutor')
 
 doc = Document(
-    content='what is the color of an apple?',
-    matches=[
-        Document(text='Apple is a pome fruit that is red in color.'),
-        Document(text='Banana is yellow.')
-    ]
+    text='Apple is a pome fruit that is red in color. Banana is yellow.',
+    tags={'question': 'what is the color of an apple?'},
 )
 
 with f:
     doc = f.search(inputs=doc, return_results=True).docs[0]
 ```
 
-After search, to get let's say the top 1 answer, paragraph where top 1 answer is extracted from and the confidence score of the top 1 answer:
-
+After searching, we can perform the following to get the top 1 answer.
 ```python
 # Top 1 match
 match = doc.matches[0]
 answer = match.text
-paragraph = match.tags['paragraph']
 confidence = match.scores['confidence'].value
 ```
+We can also get the confidence score of the answer.
 
 
 ### Use other pre-trained models
@@ -45,7 +41,7 @@ You can use other models by specifying the `model_name` parameter:
 from jina import Flow, Document
 
 f = Flow().add(
-    uses='jinahub+docker://TransformerTorchEncoder',
+    uses='jinahub+docker://TransformerQAExecutor',
     uses_with={'model_name': 'distilbert-base-cased-distilled-squad'}
 )
 ```
@@ -59,14 +55,14 @@ You may use your own tokenizer by specifying the `tokenizer_name` parameter:
 from jina import Flow, Document
 
 f = Flow().add(
-    uses='jinahub+docker://TransformerTorchEncoder',
+    uses='jinahub+docker://TransformerQAExecutor',
     uses_with={'tokenizer_name': 'distilbert-base-cased-distilled-squad'}
 )
 ```
 
 ### Use GPUs
-To enable GPU, you can set the `device` parameter to a cuda device.
-Make sure your machine is cuda-compatible.
+To enable GPU, you can set the `device` parameter to a CUDA device.
+Make sure your machine is CUDA-compatible and that you've installed all the required libraries and drivers.
 If you're using a docker container, make sure to add the `gpu` tag and enable 
 GPU access to Docker with `gpus='all'`.
 Furthermore, make sure you satisfy the prerequisites mentioned in 
--- tests/unit/test_qa_executor.py
+++ tests/unit/test_qa_executor.py
@@ -6,72 +6,57 @@ from transformer_qa import TransformerQAExecutor
 
 @pytest.fixture
 def docs():
+    paragraphs = [
+        'Document is basic data type in Jina. Flow ties Executors in a pipeline. ',
+        'Jina supports Windows. Jina supports UNIX. ',
+        'Jina supports Kubernetes. Also supports GPU. ',
+        'Executor is how Jina processes Documents. DocumentArrayMemmap stores data on-disk.',
+    ]
+
+    questions = [
+        'What is Document in Jina?',
+        'Does Jina work on Windows?',
+        'Does Jina support GPU?',
+        'What is Executor?',
+    ]
+
+    expected = [
+        'basic data type',
+        'Jina supports Windows',
+        'Also supports GPU',
+        'how Jina processes Documents',
+    ]
+
     docs = DocumentArray(
         [
-            Document(
-                text='What is Document in Jina', tags={'expected': 'basic data type'}
-            ),
-            Document(
-                text='Does Jina work on Windows',
-                tags={'expected': 'Jina supports Windows'},
-            ),
-            Document(
-                text='Does Jina support GPU', tags={'expected': 'Also supports GPU'}
-            ),
-            Document(
-                text='What is Executor',
-                tags={'expected': 'how Jina processes Documents'},
-            ),
+            Document(text=p, tags={'question': q, 'expected': e})
+            for p, q, e in zip(paragraphs, questions, expected)
         ]
     )
-    matches = DocumentArray(
-        [
-            Document(
-                text='Document is basic data type in Jina. Flow ties Executors in a pipeline.',
-                scores={'cosine': 1, 'uri_cosine': 1},
-            ),
-            Document(
-                text='Jina supports Windows. Jina supports UNIX.',
-                scores={'cosine': 1, 'uri_cosine': 1},
-            ),
-            Document(
-                text='Jina supports Kubernetes. Also supports GPU',
-                scores={'cosine': 1, 'uri_cosine': 1},
-            ),
-            Document(
-                text='Executor is how Jina processes Documents. DocumentArrayMemmap stores data on-disk.',
-                scores={'cosine': 1, 'uri_cosine': 1},
-            ),
-        ]
+    return DocumentArray(
+        Document(
+            text='\n'.join(paragraphs),
+            chunks=docs,
+            tags={'question': questions[-1], 'expected': expected[-1]},
+        )
     )
-    for doc in docs:
-        doc.matches.extend(matches)
-    return docs
 
 
 @pytest.mark.gpu
-def test_search_gpu(docs):
+@pytest.mark.parametrize('traversal_paths', ['r', 'r,c'])
+def test_search_gpu(docs, traversal_paths):
     # this only runs when we add gpu flag in pytest command i.e. "pytest --gpu ..."
-    qa = TransformerQAExecutor(device='cuda')
-    _test_search(docs, qa)
+    qa = TransformerQAExecutor(device='cuda', default_traversal_paths=traversal_paths)
+    _test_search(docs, qa, traversal_paths)
 
 
-def test_search(docs):
-    qa = TransformerQAExecutor()
-    _test_search(docs, qa)
+@pytest.mark.parametrize('traversal_paths', ['r', 'r,c'])
+def test_search(docs, traversal_paths):
+    qa = TransformerQAExecutor(default_traversal_paths=traversal_paths)
+    _test_search(docs, qa, traversal_paths)
 
 
-def _test_search(docs, qa):
+def _test_search(docs, qa, traversal_paths):
     qa.generate(docs)
-    for doc in docs:
+    for doc in docs.traverse_flat(traversal_paths):
         assert doc.matches[0].text == doc.tags['expected']
-
-
-def test_concatenate_matches(docs):
-    qa = TransformerQAExecutor()
-    concatenated_matches, span_range_to_match = qa._concatenate_matches(docs[0].matches)
-
-    for (span_min, span_max, idx) in span_range_to_match:
-        assert (
-            docs[0].matches[idx].text == concatenated_matches[span_min : span_max - 1]
-        )
